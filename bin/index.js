c666bee10dfc9df033fdc125e3d1ce2e826fb7e6
PracticeEveryday <developerforus@gmail.com>
2023-10-11 19:21:03 +0900

    feat: npx setting

---
+++ bin/index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+const { execSync } = require('child_process');
+
+const runCommand = (command) => {
+    try {
+        execSync(`${command}`, { stdio: 'inherit' });
+    } catch (e) {
+        console.log(`Failed to execute ${command}`, e);
+        return false;
+    }
+    return true;
+};
+
+const controllerName = process.argv[2];
+
+// const installControllerGen = `npm install controller-gen`;
+// console.log(`npm install controller-gen`);
+// const checkOut = runCommand(installControllerGen);
+// if (!checkOut) process.exit(-1);
+
+
+const generateControllerFile = `node index.js ${controllerName}`;
+console.log(`generate controller file with filename: ${controllerName}`);
+const checkOut = runCommand(generateControllerFile);
+if (!checkOut) process.exit(-1);
+
+
+console.log(`${controllerName}Controller.ts success`)
+
+
